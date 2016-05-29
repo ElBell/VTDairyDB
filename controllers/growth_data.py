4f7e4a0caaee5cfa3c1c504497cb7f314015cafa
ElBell <eleonorc@vt.edu>
2016-05-29 04:29:31 -0400

    Made a table for birth_status data on the growth data page

--- controllers/growth_data.py
+++ controllers/growth_data.py
@@ -12,5 +12,5 @@ from dateutil import parser
 def growth_data():
     growth_data_table = GrowthData.query.all()
     birth_status_data_table = BirthStatusData.query.all()
-    # total_growth_data =
-    return render_template('growth_data.html', current_table=growth_data_table)
+    total_growth_data = birth_status_data_table
+    return render_template('growth_data.html', current_table=total_growth_data)
